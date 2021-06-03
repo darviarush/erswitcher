2c3dfe651ff86219782d8cc9d4d496a1e7b096e5
darviarush <halfdroy@gmail.com>
2021-06-03 12:55:12 +0300

    master save

--- Makefile
+++ Makefile
@@ -8,4 +8,4 @@ start: erswitcher
 # 	gcc -lX11  -o $@ $^
 
 erswitcher:	src/erswitcher.c src/keyboard.c src/keyboard.h
-	gcc -lX11 -lxkbcommon -o erswitcher src/keyboard.c src/erswitcher.c
+	gcc -lX11 -lxkbcommon -lXtst -o erswitcher src/keyboard.c src/erswitcher.c
--- src/erswitcher.c
+++ src/erswitcher.c
@@ -15,14 +15,17 @@
 #define BIT(c, x)   ( c[x/8]&(1<<(x%8)) )
 
 // Объявления функций
-//int mod_pressed(char* keys);
 void print_state(XkbStateRec* state);
 Window get_current_window();
 
+// Обработка ошибок
 static int xerror = 0;
-static int *null_X_error (Display *d, XErrorEvent *e) {
+static int *null_X_error (Display *d, XErrorEvent *err) {
 	xerror = 1;
-	fprintf(stderr, "x error!\n");
+	#define BUFLEN		(1024*64)
+	char buf[BUFLEN];
+    XGetErrorText(d, err->error_code, buf, BUFLEN);
+	fprintf(stderr, "x error! %s\n", buf);
 }
 
 
@@ -30,29 +33,30 @@ Display *d;					// текущий дисплей
 Window current_win;			// окно устанавливается при вводе с клавиатуры
 
 #define SIZE	1024
-wint_t word[SIZE];
+wint_t word_buf[SIZE];
+wint_t *word = word_buf;
+wint_t trans_buf[SIZE];
+wint_t *trans = trans_buf;
 int pos = 0;
 
 
 void change_key(char* keys, int code) {
 	printf("change_key! code=%d\n", code); fflush(stdout);
 
-
-	// нажата функциональная клавиша и это не shift
-	// if(mod_pressed(keys)) return;
-
-	if(group_en == -1) group_en = 0;
-	if(group_ru == -1) group_ru = 1;
-
 	XkbStateRec state;
 	XkbGetState(d, XkbUseCoreKbd, &state);
-	print_state(&state);
+	//print_state(&state);
 
 	int shiftLevel = ((state.mods & (ShiftMask | LockMask)) 
 		&& !(state.mods & ShiftMask && state.mods & LockMask))? 1: 0;
-	printf("shiftLevel=%i\n", shiftLevel);
+	//printf("shiftLevel=%i\n", shiftLevel);
+
+	// транспонированная раскладка
+	int trans_group = state.group == group_en? group_ru: group_en;
+
 	// 8-байтный символ иксов
 	KeySym ks = XkbKeycodeToKeysym(d, code, state.group, shiftLevel);
+	KeySym ks_trans = XkbKeycodeToKeysym(d, code, trans_group, shiftLevel);
 	KeySym ks_pause = XkbKeycodeToKeysym(d, code, group_en, 0);
 
 	const char *s1 = XKeysymToString(ks);
@@ -67,22 +71,29 @@ void change_key(char* keys, int code) {
 	}
 
 	wint_t cs = xkb_keysym_to_utf32(ks);
+	wint_t cs_trans = xkb_keysym_to_utf32(ks_trans);
+	wint_t cs_pause = xkb_keysym_to_utf32(ks_pause);
 
 	printf ("0x%04llx -> 0x%04lx: %s `%C` en: %s\n", ks, cs, s1, cs, s_pause);
 
 	// BackSpace - удаляем последний символ
-	if(cs == 8) {
+	if(ks_pause == 0xff08) {
 		if(pos != 0) --pos;
+		word[pos] = trans[pos] = 0;
+		printf("backspace: %S -> %S\n", word, trans);
 		return;
 	}
 
 	// нажата Pause - переводим
 	if(ks_pause == 0xff13) {
-		// Если нечего переводить, то показываем сообщение на месте ввода
-		if(pos == 0) { return; }
 
-		// на какую клаву переводим
-		int group = state.group == group_ru? group_en: group_ru;
+		word[pos] = trans[pos] = 0;
+		printf("pause! %S -> %S\n", word, trans);
+
+		// Если нечего переводить, то показываем сообщение на месте ввода
+		if(pos == 0) {
+			//...
+		}
 
 		// отправляем бекспейсы, чтобы удалить ввод
 		for(int i=0; i<pos; i++) {
@@ -91,25 +102,34 @@ void change_key(char* keys, int code) {
 
 		// вводим ввод, но в альтернативной раскладке
 		for(int i=0; i<pos; i++) {
-			wchar_t uk = translate(word[i]);
-			press_key(uk);
+			press_key(trans[i]);
 		}
 
+		// меняем буфера местами
+		wint_t *x = trans;
+		trans = word;
+		word = x;
+
 		// меняем раскладку
-		set_group(group);
+		set_group(trans_group);
 		return;
 	}
 
 	//if(cs == 0) { pos = 0; return; } // разные управляющие клавиши
 	//if(iswspace(cs)) { pos = 0;	return;	}
 	// Если символ не печатный, то начинаем ввод с начала
-	if(!iswprint(cs)) {	pos = 0; return; }
+	if(!iswprint(cs)) {	
+		//pos = 0; 
+		printf("not isprint! `%C`\n", cs);
+		return; 
+	}
 
 	// Записываем символ в буфер с его раскладкой клавиатуры
 	if(pos >= SIZE) pos = 0;
+	trans[pos] = cs_trans;
 	word[pos++] = cs;
-	word[pos] = 0;
-	printf("len=%i s=%S\n", pos, word);	fflush(stdout);
+	trans[pos] = word[pos] = 0;
+	printf("press! %S -> %S\n", word, trans);
 }
 
 
@@ -137,6 +157,7 @@ void main(int ac, char** av) {
 	// Начальные установки
 	current_win = get_current_window();
 	pos = 0;
+	keysym_init();
 
 	char buf1[32], buf2[32];
 	char *saved=buf1, *keys=buf2;
@@ -162,17 +183,6 @@ void main(int ac, char** av) {
    	}
 }
 
-// Переключение раскладки
-// group - номер раскладки (обычно порядковый номер 
-//         в списке включенных раскладок, счет с 0
-void set_active_group(int group) {
-    // Отправка запроса на переключение раскладки
-    XkbLockGroup(d, XkbUseCoreKbd, group);
-
-    // Вызов `XkbGetState()` (получение состояния клавиатуры) для выполнения запроса, без этого вызова переключение раскладки не сработает
-    XkbStateRec state;
-    XkbGetState(d, XkbUseCoreKbd, &state);
-}
 
 // возвращает текущее окно
 Window get_current_window() {
@@ -180,69 +190,6 @@ Window get_current_window() {
   	int revert_to = 0;
 	XGetInputFocus(d, &w, &revert_to);
 	return w;
-}
-
-// транслитерирует текст с русской раскладки в английскую
-static wint_t *ru = L"ё1234567890-="
-					 L"йцукенгшщзхъ"
-					 L"фывапролджэ\\"
-					 L"/ячсмитьбю."
-					 ;
-static wint_t *en = L"`1234567890-=" 
-					 L"qwertyuiop[]"
-					 L"asdfghjkl;'\\"
-					 L"<zxcvbnm,./"
-					 ;
-void translation(wint_t *s) {
-
-}
-
-// возвращает номер раскладки по имени: en или ru
-int get_group_by_name(char* name) {
-	XkbDescRec* kb = XkbAllocKeyboard();
-	if(!kb) return -1;
-
-	kb->dpy = d;
-	if(XkbGetNames(d, XkbGroupNamesMask, kb) != Success) return -1;
-
-	Atom* gs = kb->names->groups;
-	for(int i = 0; i < XkbNumKbdGroups && gs[i] != 0; ++i) {
-		char* kb_name = XGetAtomName(d, gs[i]);
-		//printf("kb_name: %s\n", kb_name);
-		if(strncmp(kb_name, name, strlen(name)) == 0) return i;
-	}
-	
-	XkbFreeNames(kb, XkbGroupNamesMask, 0);
-
-	return -1;
-}
-
-/* отправляет символ в активное окно
- * 
- * code - номер клавиши в клавиатуре
- * group - раскладка клавиатуры
- * mods - какие модифицирующие клавиши нажаты
- * is_press - клавиша нажата или отжата
- */
-void send_key_event(int code, int group, int mods, int is_press) {
-	XKeyEvent xk;
-
-	xk.display = d;
-	xk.subwindow = None;
-	xk.time = CurrentTime;
-	xk.same_screen = True;
-	xk.send_event = True; // событие послано другим клиентом через вызов XSendEvent
-
-	// Позиция курсора мыши в окне и на экране
-	xk.x = xk.y = xk.x_root = xk.y_root = 1;
-
-	xk.window = current_win;
-    xk.keycode = code;
-    xk.state = mods | (group << 13);
-    xk.type = is_press? KeyPress: KeyRelease;
-    XSendEvent(d, xk.window, True, KeyPressMask, (XEvent *)&xk);
-
-    XFlush(d);
 }
 
 // печатает в stdout состояние клавиатуры
@@ -262,6 +209,79 @@ void print_state(XkbStateRec* state) {
 	printf("unsigned char	compat_lookup_mods: %i\n", (unsigned int) (state->compat_lookup_mods));
 	printf("unsigned short	ptr_buttons: %i\n", (unsigned int) (state->ptr_buttons));
 }
+
+// // транслитерирует текст с русской раскладки в английскую
+// static wint_t *ru = L"ё1234567890-="
+// 					L"йцукенгшщзхъ"
+// 					L"фывапролджэ\\"
+// 					L"/ячсмитьбю."
+// 					;
+// static wint_t *en = L"`1234567890-=" 
+// 					L"qwertyuiop[]"
+// 					L"asdfghjkl;'\\"
+// 					L"<zxcvbnm,./"
+// 					;
+// static wint_t *RU = L"Ё!\"№;%:?*()_+"
+// 					L"ЙЦУКЕНГШЩЗХЪ"
+// 					L"ФЫВАПРОЛДЖЭ/"
+// 					L"|ЯЧСМИТЬБЮ,"
+// 					;
+// static wint_t *EN = L"~!@#$%^&*()_+" 
+// 					L"QWERTYUIOP{}"
+// 					L"ASDFGHJKL:\"|"
+// 					L">ZXCVBNM<>?"
+// 					;
+// void translation(wint_t *s, int from, int to) {
+
+// }
+
+// // возвращает номер раскладки по имени: en или ru
+// int get_group_by_name(char* name) {
+// 	XkbDescRec* kb = XkbAllocKeyboard();
+// 	if(!kb) return -1;
+
+// 	kb->dpy = d;
+// 	if(XkbGetNames(d, XkbGroupNamesMask, kb) != Success) return -1;
+
+// 	Atom* gs = kb->names->groups;
+// 	for(int i = 0; i < XkbNumKbdGroups && gs[i] != 0; ++i) {
+// 		char* kb_name = XGetAtomName(d, gs[i]);
+// 		//printf("kb_name: %s\n", kb_name);
+// 		if(strncmp(kb_name, name, strlen(name)) == 0) return i;
+// 	}
+	
+// 	XkbFreeNames(kb, XkbGroupNamesMask, 0);
+
+// 	return -1;
+// }
+
+// /* отправляет символ в активное окно
+//  * 
+//  * code - номер клавиши в клавиатуре
+//  * group - раскладка клавиатуры
+//  * mods - какие модифицирующие клавиши нажаты
+//  * is_press - клавиша нажата или отжата
+//  */
+// void send_key_event(int code, int group, int mods, int is_press) {
+// 	XKeyEvent xk;
+
+// 	xk.display = d;
+// 	xk.subwindow = None;
+// 	xk.time = CurrentTime;
+// 	xk.same_screen = True;
+// 	xk.send_event = True; // событие послано другим клиентом через вызов XSendEvent
+
+// 	// Позиция курсора мыши в окне и на экране
+// 	xk.x = xk.y = xk.x_root = xk.y_root = 1;
+
+// 	xk.window = current_win;
+//     xk.keycode = code;
+//     xk.state = mods | (group << 13);
+//     xk.type = is_press? KeyPress: KeyRelease;
+//     XSendEvent(d, xk.window, True, KeyPressMask, (XEvent *)&xk);
+
+//     XFlush(d);
+// }
 
 // // выводит подсказку с текстом
 // void show_hint(wchar_t *s) {
