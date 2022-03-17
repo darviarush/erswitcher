d7005ac42254db76b720098e9db9cb86922da3b7
Yaroslav O. Kosmina <darviarush@mail.ru>
2022-03-17 19:40:16 +0300

    master Конфигуратор получил свой конфиг с положением окна + Закладки для быстрого доступа к мнемоникам

--- erswitcher-configurator.tcl
+++ erswitcher-configurator.tcl
@@ -4,14 +4,6 @@ package require Tk
 
 tk appname "En-Ru Switcher Конфигуратор"
 
-# центрируем окно
-set width 600
-set height 500
-set x [expr { ( [winfo vrootwidth  .] - $width  ) / 2 }]
-set y [expr { ( [winfo vrootheight .] - $height ) / 2 }]
-puts ${width}x${height}+${x}+${y}
-wm geometry . ${width}x${height}+${x}+${y}
-
 #@category виджеты
 
 # делаем вертикальный скроллбар. f - фрейм с виджетом w для которого скроллбар и делается 
@@ -25,6 +17,10 @@ proc make_scrolled_y {f w} {
 
 
 #@category файлы
+
+proc exists_file {path} {
+	return [expr {![catch {file lstat $path finfo}]}]
+}
 
 proc read_file {path} {
 	set f [open $path r]
@@ -81,22 +77,62 @@ proc ::tk_textPaste w {
 # }
 
 #@category инициализация
- 
+
+set bookmarks []
+
+pack [panedwindow .m -orient horizontal] -fill both -expand 1
+
+pack [make_scrolled_y [frame .l] [listbox .l.list -selectmode single]] -side left -fill y
 pack [make_scrolled_y [frame .t] [text .t.text -wrap word -undo 1 -maxundo -1]] -fill both -expand 1
 
+.m add .l
+.m add .t
+
+.t.text tag configure bookmark -foreground #DC143C
 .t.text tag configure remark -foreground #008080
 .t.text tag configure section -foreground #DC143C
 .t.text tag configure equal -foreground #1E90FF
 .t.text tag configure error -background #DC143C -foreground white
 
+# центрируем окно
+set path_wmconf "~/.config/erswitcher-configurator.conf"
+set wmconf ""
+catch {set wmconf [read_file $path_wmconf]}
+
+if {$wmconf == ""} {
+	set width 800
+	set height 500
+	set x [expr { ( [winfo vrootwidth  .] - $width  ) / 2 }]
+	set y [expr { ( [winfo vrootheight .] - $height ) / 2 }]
+	puts ${width}x${height}+${x}+${y}
+	wm geometry . ${width}x${height}+${x}+${y}
+} else {
+	regexp {^(\d+) (.*?)\n} $wmconf -> width geometry
+
+	wm geometry . $geometry
+	.m paneconfigure .l -width $width
+}
+
+wm protocol . WM_DELETE_WINDOW {
+	write_file $path_wmconf "[winfo width .l] [wm geometry .]"
+	destroy .
+}
+
+# показываем конфиг
 set path "~/.config/erswitcher.conf"
 set conf [read_file $path]
 .t.text insert end $conf
 
+# колоризирует код и переписывает закладки
 proc coloring {text} {
-	foreach i {remark section equal error} {
+	# удаляем теги
+	foreach i {bookmark remark section equal error} {
 		.t.text tag remove $i 1.0 end
 	}
+	# удаляем закладки
+	global bookmarks
+	set bookmarks []
+	.l.list delete 0 end
 	
 	set re {^((?:\\.|[^\\])*)=}
 	
@@ -105,7 +141,17 @@ proc coloring {text} {
 		incr lineno
 		
 		switch -regexp -- $line {
-			{^\#} { .t.text tag add remark $lineno.0 $lineno.end }
+			{^## } {
+				# закладки
+				set is [regexp {^\#\#\s+(.+)} $line -> s]
+				if {$is} {
+					lappend bookmarks $lineno
+					.l.list insert end $s
+				}
+				# подсветка комментария в тексте
+				.t.text tag add bookmark $lineno.0 $lineno.end
+			}
+			{^\#} {	.t.text tag add remark $lineno.0 $lineno.end }
 			{^\[(functions|options|compose|snippets|commands|sendkeys)\]\s*$} { .t.text tag add section $lineno.0 $lineno.end }
 			{^\s*$} {}
 			{=} {
@@ -133,3 +179,12 @@ bind .t.text <KeyRelease> {
 	puts "Сигнал отправлен erswitcher-у с результатом: $res ($result)"
 }
 
+# при выборе закладки переходим к ней
+bind .l.list <<ListboxSelect>> {
+	puts [list %W %T [%W index active] [%W curselection] [%W index anchor] ]
+	set idx [%W curselection]
+	if {$idx != ""} {
+		set pos [lindex $bookmarks $idx]
+		.t.text see "$pos.0"		
+	}
+}
